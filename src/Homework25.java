2580d307404c4355691bee746edbbd4956f8dd59
nadja <nadja.kalenda31@gmail.com>
2024-06-05 17:03:52 +0200

    kl

---
+++ src/Homework25.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Homework25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        string.split(" ");
+
+        String number1 = "4 + 4";
+        Integer.parseInt("4");
+        String number2 = "23 * 78";
+        Integer.parseInt("23"); Integer.parseInt("78");
+        String number3 = "12 / 45";
+        Integer.parseInt("12"); Integer.parseInt("45");
+        String number4 = "3 - 7";
+        Integer.parseInt("5"); Integer.parseInt("7");
+
+        if(string.equals("*")){
+    System.out.println(number1);
+
+//        }
+
+
+
+//        String[] numbers = {"3", "4", "7", "12", "23", "45", "78"};
+//        for (int index = 0; index < 6; index++) {
+//            Integer.parseInt(numbers[index]);
+//        }
+//    }
+//
+//
+//    public static int sum(int[] a, int[] b) {
+//        int c = a[1] + b[1];
+//        return c;
+//    }
+//
+//    public static int multipl(int[] a, int[] b) {
+//        int c = a[4] * b[6];
+//        return c;
+//    }
+//
+//
+//    public static int diff(int[] a, int[] b) {
+//        int c = a[3] / b[5];
+//        return c;
+//    }
+//
+//
+//    public static int substr(int[] a, int[] b) {
+//        int c = a[0] - b[2];
+//        return c;
+    }
+
+
+
+
+
+}
+
+
+
+//2.
+//Пользователь вводит строку вида:
+//число знак число
+//Выведите ответ на выражение
+//4 + 4
+//23 * 78
+//12 / 45
+//3 - 7
+
+//Integer.parseInt, split, equals
