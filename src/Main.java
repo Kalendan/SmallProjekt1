5b71ae7d7bc45442c7d521b1872e449afe31ece8
nadja <nadja.kalenda31@gmail.com>
2024-06-05 17:03:19 +0200

    kl

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String words = "It is raining all day!";
+        String[] array = words.split(" ");
+        String result = " ";
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            int size = words.length();
+            String first = array[i].substring(0, 1);
+            String result1 = first;
+            sum += first;
+
+
+            System.out.println(array[i]);
+            System.out.println(first);
+            System.out.println(result1);
+            System.out.println(sum);
+
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+            //homework 04.05
+
+            //1.
+//Пользователь вводит строку
+//Разделите строку на слова
+//В каждом слове оставьте только первую букву// чар
+//Сложите все первые буквы слов в одно слово
+//Напишите на экран размер получившейся строки, а также первую и последнюю буквы слова
+
+
